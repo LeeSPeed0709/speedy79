56095330fa5224ba95027083b17ea8152d7c5892
leespeed <li.shipeng@huadongmedia.com>
2019-05-06 18:19:40 +0800

    save

--- pages/detail.js
+++ pages/detail.js
@@ -4,48 +4,83 @@ import Head from '../components/head'
 
 export default class extends React.Component {
     static async getInitialProps({ query }) {
-        return { name: query.id };
+        return { name: query.name };
+    }
+
+    state = {
+        data: {}
+    }
+
+    componentDidMount() {
+        this.connectWebSocket();
+    }
+
+    connectWebSocket() {
+        this.ws = new WebSocket(`wss://stream.binance.cloud:9443/ws/${this.props.name.toLowerCase()}@depth20`);
+
+        this.ws.onopen = () => {
+            console.log('open');
+            this.setState({ isConnecting: true });
+        }
+
+        this.ws.onmessage = res => {
+            this.setState({
+                data: JSON.parse(res.data)
+            })
+        }
+
+        this.ws.onclose = () => {
+            console.log('close');
+            this.setState({ isConnecting: false });
+        }
+    }
+
+    breakConnect() {
+        this.ws.close();
     }
 
     render() {
         let { name } = this.props;
+        let { data, isConnecting } = this.state;
         return (
             <div>
                 <Head title={name} />
+                {
+                    isConnecting ?
+                        <button onClick={this.breakConnect.bind(this)}>断开</button> :
+                        <button onClick={this.connectWebSocket.bind(this)}>连接</button>
+                }
                 <div>{name}</div>
-                <Link href="/">
+                <div>买</div>
+                {
+                    data.bids && data.bids.map((item, index) => {
+                        return (
+                            <div key={`${index}-${new Date().getTime()}`} className="section"><div className="price">{item[0]}</div><div className="quantity">{item[1]}</div></div>
+                        )
+                    })
+                }
+                <div>卖</div>
+                {
+                    data.asks && data.asks.map((item, index) => {
+                        return (
+                            <div key={`${index}-${new Date().getTime()}`} className="section"><div className="price">{item[0]}</div><div className="quantity">{item[1]}</div></div>
+                        )
+                    })
+                }
+                {/* <Link href="/">
                     <a>回主页</a>
-                </Link>
+                </Link> */}
                 <style jsx>{`
-                    ul {
-                        width: 1200px;
-                        margin: 0 auto;
-                        padding: 0;
-                        list-style: none;
-                        border-bottom: 1px solid #ccc;
-                    }
-                    li {
-                        height: 29px;
-                        line-height: 29px;
-                        border-top: 1px solid #ccc;
-                        color: #999;
-                    }
-                    a{
-                        text-decoration: none;
-                        color: #333;
-                    }
-                    .row {
-                        display: flex;
-                    }
-                    .row:hover {
-                        background: rgb(253, 247, 236);
-                    }
-                    .cell {
-                        flex: 1;
-                    }
-                    .right {
-                        text-align: right;
-                    }
+                .section {
+                    overflow: hidden;
+                    width: 300px;
+                }
+                .price {
+                    float: left;
+                }
+                .quantity {
+                    float: right;
+                }
                 `}</style>
             </div>
         )
--- pages/index.js
+++ pages/index.js
@@ -28,59 +28,59 @@ export default class extends React.Component {
                         <div className="cell right">24h最低价</div>
                         <div className="cell right">24h成交量</div>
                     </li>
-                {
-                    data && data.length > 0 && data.map(item => {
-                        return (
-                            <li key={item.symbol}>
-                                <Link href={`/detail?id=${item.baseAsset}_${item.quoteAsset}`}>
-                                    <a className="row">
-                                        <div className="cell">{item.baseAsset}/{item.quoteAsset}</div>
-                                        <div className="cell">{item.quoteAssetName}</div>
-                                        <div className="cell right">{item.high}</div>
-                                        <div className="cell right">{item.low}</div>
-                                        <div className="cell right">{item.tradedMoney}</div>
-                                    </a>
-                                </Link>
-                            </li>
-                        )
-                    })
-                }
+                    {
+                        data && data.length > 0 && data.map(item => {
+                            return (
+                                <li key={item.symbol}>
+                                    <Link href={`/detail?name=${item.symbol}`}>
+                                        <a className="row">
+                                            <div className="cell">{item.baseAsset}/{item.quoteAsset}</div>
+                                            <div className="cell">{item.quoteAssetName}</div>
+                                            <div className="cell right">{item.high}</div>
+                                            <div className="cell right">{item.low}</div>
+                                            <div className="cell right">{item.tradedMoney}</div>
+                                        </a>
+                                    </Link>
+                                </li>
+                            )
+                        })
+                    }
                 </ul>
                 <style jsx>{`
-                    :global(body) {
-                        margin: 0;
-                        font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir,
-                        Helvetica, sans-serif;
-                    }
-                    ul {
-                        width: 1200px;
-                        margin: 0 auto;
-                        padding: 0;
-                        list-style: none;
-                        border-bottom: 1px solid #ccc;
-                    }
-                    li {
-                        height: 29px;
-                        line-height: 29px;
-                        border-top: 1px solid #ccc;
-                        color: #999;
-                    }
-                    a{
-                        text-decoration: none;
-                        color: #333;
-                    }
-                    .row, .row-head {
-                        display: flex;
-                    }
-                    .row:hover {
-                        background: rgb(253, 247, 236);
-                    }
-                    .cell {
-                        flex: 1;
-                    }
-                    .right {
-                        text-align: right;
-                    }
+                :global(body) {
+                    margin: 0;
+                    font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir,
+                    Helvetica, sans-serif;
+                }
+                ul {
+                    width: 1200px;
+                    margin: 0 auto;
+                    padding: 0;
+                    list-style: none;
+                    border-bottom: 1px solid #ccc;
+                }
+                li {
+                    height: 29px;
+                    line-height: 29px;
+                    border-top: 1px solid #ccc;
+                    color: #999;
+                }
+                a{
+                    text-decoration: none;
+                    color: #333;
+                }
+                .row, .row-head {
+                    display: flex;
+                }
+                .row:hover {
+                    background: rgb(253, 247, 236);
+                }
+                .cell {
+                    flex: 1;
+                }
+                .right {
+                    text-align: right;
+                }
                 `}</style>
             </div>
         )
